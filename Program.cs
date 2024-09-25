019a4d30d89c70a0ebe08488a974e7c81e1b2992
Bohdan <bgavlovskiy@gmail.com>
2024-09-26 01:52:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+int n = int.Parse(Console.ReadLine());
+Fibonacci.Write(n);
+Console.ReadLine();
+public class Fibonacci
+{
+    public static void Write(int n)
+    {
+        Console.WriteLine(0);
+        Console.WriteLine(1);
+        int X1 = 0;
+        int X2 = 1;
+        while (X2<n)
+        {
+            int Output = X1 + X2;
+            X1 = X2;
+            X2 = Output;
+            if (X2 <= n)
+            {
+                Console.WriteLine(X2);
+            }
+        }
+        return;
+    }
+}
